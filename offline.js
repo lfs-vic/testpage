1753d6edb38eb4318f1a90b8f84864ea55dce60f
vic-lionfish <victor@lionfish-studios.com>
2023-01-04 14:45:44 +0800

    bargames upload test

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1672814671,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/gametitle-sheet0.png",
+		"images/instructions-sheet0.png",
+		"images/startbutton-sheet0.png",
+		"images/homebutton-sheet0.png",
+		"images/promptbox-sheet0.png",
+		"images/return-sheet0.png",
+		"images/title-sheet0.png",
+		"images/drinkup-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"cavilant.otf"
+	]
+}
